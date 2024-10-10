91f7d16b20a01ce92f865187b443ec1f6bb97a6f
tixalim <rosalie.musilova@gmail.com>
2024-10-10 16:58:13 +0200

    #1 dopsani ui

--- IS Projekty/program001-vypis-rady/Program.cs
+++ IS Projekty/program001-vypis-rady/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿class Program {
+
+    static void Main() {
+
+        string again = "a";
+        while (again == "a"){
+
+        Console.Clear();
+        Console.WriteLine("****************************");
+        Console.WriteLine("**** Výpis číselné řady ****");
+        Console.WriteLine("*********** Rosa ***********");
+        Console.WriteLine("****************************");
+        Console.WriteLine();
+        Console.WriteLine();
+        // Vstup od uživatele
+        /* Console.Write("Zadejte první číslo (celé číslo): ");
+        int first = int.Parse(Console.ReadLine()); */
+        
+        // Vstup od uživatele  - lépe řešený
+        Console.Write("Zadejte první číslo (celé číslo): ");
+        int first;
+        while(!int.TryParse(Console.ReadLine(), out first)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+        }
+        Console.Write("Zadejte poslední číslo (celé číslo): ");
+        int last;
+        while(!int.TryParse(Console.ReadLine(), out last)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo znovu: ");
+        }
+        Console.Write("Zadejte diferenci (celé číslo): ");
+        int step;
+        while(!int.TryParse(Console.ReadLine(), out step)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
+        }
+        
+        Console.WriteLine("==============================");
+        Console.WriteLine("První číslo: {0}", first);
+        Console.WriteLine("Druhé číslo: {0}", last);
+        Console.WriteLine("Diference: {0}", step);
+
+
+
+
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Program můžete opakovat stisknutím klávesy 'a'");
+        again = Console.ReadLine();
+        
+        
+
+        }
+        Console.ReadKey();
+       
+    }
+    
+} 
--- IS Projekty/program001-vypis-rady/obj/Debug/net7.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS Projekty/program001-vypis-rady/obj/Debug/net7.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c5eb0b6b83e97e97aa1f68be7444e72240f1ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe8a33b59e40b43069d3d9aa23bb26718a500ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
