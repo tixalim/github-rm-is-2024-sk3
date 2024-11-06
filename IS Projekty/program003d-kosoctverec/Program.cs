7149cc322ccb691acd2adc4a2d60b620aceedc0f
tixalim <rosalie.musilova@gmail.com>
2024-11-06 19:16:51 +0100

    #4 kosoctverec

---
+++ IS Projekty/program003d-kosoctverec/Program.cs
@@ -0,0 +1,36 @@
+﻿string again = "a";
+    while(again == "a") {
+
+        
+        Console.Write("Zadejte vysku obrazce (cele cislo) ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+                Console.Write("Nezadali cele cislo, zadejte znovu:");
+            }
+        
+        for (int i = 0; i < height; i++)
+        {
+            int mezery = Math.Abs(i - height / 2);
+
+            for (int j = 0; j < mezery; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < height - 2 * mezery; j++)
+            {
+                Console.Write("*");
+            }
+            for (int j = 0; j < mezery; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+    again = Console.ReadLine();    
+
+}
+
