0366a04964bbbd0833b61a911f07da8cd77e84fd
tixalim <rosalie.musilova@gmail.com>
2024-11-06 19:26:03 +0100

    #4 pismeno Z

---
+++ IS Projekty/program003e-zed/Program.cs
@@ -0,0 +1,37 @@
+﻿string again = "a";
+    while(again == "a") {
+
+        
+        Console.Write("Zadejte vysku obrazce (cele cislo) ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+                Console.Write("Nezadali cele cislo, zadejte znovu:");
+            }
+        for (int i = 1; i <= height; i++)
+        {
+            for (int j = 1; j <= height; j++)
+            {
+                if (i == 1 || i == height)
+                {
+                    Console.Write("* ");
+                }
+                else if (j == height - i + 1)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+    again = Console.ReadLine();    
+
+}
+
+
