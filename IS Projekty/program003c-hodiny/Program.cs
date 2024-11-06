7860de59a8690fec7040c21cd4b9d8069478c9cc
tixalim <rosalie.musilova@gmail.com>
2024-11-06 19:07:07 +0100

    #4 presypaci hodiny

---
+++ IS Projekty/program003c-hodiny/Program.cs
@@ -0,0 +1,46 @@
+﻿string again = "a";
+    while(again == "a") {
+
+        
+        Console.Write("Zadejte vysku obrazce (cele cislo) ");
+            int height;
+            while(!int.TryParse(Console.ReadLine(), out height)) {
+                Console.Write("Nezadali cele cislo, zadejte znovu:");
+            }
+        
+        int mid = height / 2; 
+
+        for (int i = 0; i <= mid; i++)
+        {
+            int mezery = i;
+            for (int j = 0; j < mezery; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < height - 2 * mezery; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        for (int i = mid - 1; i >= 0; i--)
+        {
+            int mezery = i;
+            for (int j = 0; j < mezery; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j < height - 2 * mezery; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+    again = Console.ReadLine();    
+
+}
+
