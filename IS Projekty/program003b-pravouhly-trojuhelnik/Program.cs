052a9dd6448e989eff593a28316199c385f9f829
tixalim <rosalie.musilova@gmail.com>
2024-11-06 17:19:22 +0100

    #4 trojuhelnikx

---
+++ IS Projekty/program003b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,24 @@
+﻿string again = "a";
+    while(again == "a") {
+
+        Console.WriteLine("Zadejte vysku trojuhelniku (cele cislo): ");
+        uint wigth;
+        while(!uint.TryParse(Console.ReadLine(), out wigth)){
+            Console.WriteLine("Nezadali jste cele cislo, zadejte znovu: ");
+        }
+
+        for (int i = 1; i <= wigth; i++)
+        {      
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+    again = Console.ReadLine();    
+
+}
