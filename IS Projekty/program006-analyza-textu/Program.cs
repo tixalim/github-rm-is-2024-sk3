1d53189ff2324c5f355a7669357c4a4be1dba266
tixalim <rosalie.musilova@gmail.com>
2024-11-07 17:03:13 +0100

    #7 analyza textu

---
+++ IS Projekty/program006-analyza-textu/Program.cs
@@ -0,0 +1,62 @@
+﻿string again = "a";
+        while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("************** Analýza textu ************");
+            Console.WriteLine("****************** Rose *****************");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Zadejte text pro nalyzu: ");
+            string mujText = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(mujText);
+            Console.WriteLine(mujText[0]);
+            Console.WriteLine(mujText.Length);
+            Console.WriteLine(mujText[mujText.Length]);
+
+            string samohlasky = "aáeéiíoóuúůyý";
+            string souhlasky = "bcčdďfghijklmňnpqrřsštťvwxzž";
+            string cislice = "0123456789";
+
+            int pocetSouhlasek = 0;
+            int pocetSamohlasek = 0;
+            int pocetCislic = 0;
+            int pocetOstatnich = 0;
+
+            foreach(char znak in mujText){
+                if (samohlasky.Contains(znak)){
+
+            }
+            else if(souhlasky.Contains(znak)){
+
+            }
+            else if(cislice.Contains(znak)){
+
+            }
+            else
+                pocetOstatnich++;
+            }
+            
+            Console.WriteLine("\n\nPocet samohlasek je: {0}", pocetSamohlasek);
+            Console.WriteLine("Pocet souhlasek je: {0}", pocetSouhlasek);
+            Console.WriteLine("Pocet cislic je: {0}", pocetCislic);
+            Console.WriteLine("Pocet ostatnich znaku je: {0}", pocetOstatnich);
+
+
+
+
+
+            
+            
+           
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine();    
+
+        }
